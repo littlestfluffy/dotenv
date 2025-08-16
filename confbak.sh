b7ebf27c24db7d64128ff8883e1d6b72f63da959
woosting <github@woosting.nl>
2025-08-16 03:46:01 +0200

    StoreBackup.conf added to confbak.sh

--- confbak.sh
+++ confbak.sh
@@ -4,7 +4,11 @@ DESTINATIONDIR="/tmp"
 
 NOW=$(date +"%Y%m%dt%H%M%S")
 FILENAME="${HOSTNAME}-confs.${NOW}"
-CONFIGS=(.config/hypr/rules.conf .config/hypr/settings.conf)
+CONFIGS=(
+ .config/hypr/rules.conf
+ .config/hypr/settings.conf
+ .config/storeBackup/storeBackup.conf
+)
 
 echo "Backupping to ${DESTINATIONDIR}:"
 tar -cvf ${DESTINATIONDIR}/${FILENAME}.tar ${CONFIGS[@]}
